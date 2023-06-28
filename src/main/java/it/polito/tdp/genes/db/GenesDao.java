669079e017813044328fe6418ffd74645c34320b
gianmarcocarlone <gianmarcocarlone@localhost>
2023-06-28 22:19:06 +0200

    -

--- src/main/java/it/polito/tdp/genes/db/GenesDao.java
+++ src/main/java/it/polito/tdp/genes/db/GenesDao.java
@@ -14,7 +14,8 @@ import it.polito.tdp.genes.model.Interactions;
 
 public class GenesDao {
 	
-	public List<Genes> getAllGenes(){
+	public List<Genes> getAllGenes(Map<String,Genes>idMap){
+		
 		String sql = "SELECT DISTINCT GeneID, Essential, Chromosome FROM Genes";
 		List<Genes> result = new ArrayList<Genes>();
 		Connection conn = DBConnect.getConnection();
@@ -27,7 +28,11 @@ public class GenesDao {
 				Genes genes = new Genes(res.getString("GeneID"), 
 						res.getString("Essential"), 
 						res.getInt("Chromosome"));
+				
 				result.add(genes);
+				
+				
+				idMap.put(res.getString("GeneID"), genes); 
 			}
 			res.close();
 			st.close();
@@ -80,44 +85,10 @@ public class GenesDao {
 			PreparedStatement st = conn.prepareStatement(sql);
 			st.setString(1,local);
 			ResultSet res = st.executeQuery();
+			
 			while (res.next()) {
-//
-//				Genes genes = new Genes(res.getString("GeneID"), 
-//						res.getString("Essential"), 
-//						res.getInt("Chromosome"));
-				
 				result.add(res.getString("GeneID"));
 			}
-			res.close();
-			st.close();
-			conn.close();
-			return result;
-			
-		} catch (SQLException e) {
-			throw new RuntimeException("Database error", e) ;
-		}
-	}
-	
-public List<String>  getPeso (String gId1, String gId2) {
-		
-		String sql = " select distinct  i.`Type` "
-				+ "from interactions i, genes g1, genes g2 "
-				+ "where (g1.`GeneID`= i.`GeneID1` and g2.`GeneID`= i.`GeneID2`) or (g1.`GeneID`= i.`GeneID2` and g2.`GeneID`= i.`GeneID1`) and "
-				+ "g1.`GeneID`= ? and g2.`GeneID`= ? ";
-		
-		List<String> result = new ArrayList<>();
-		Connection conn = DBConnect.getConnection();
-
-		try {
-			PreparedStatement st = conn.prepareStatement(sql);
-			st.setString(1,gId1); 
-			st.setString(2,gId1);
-
-			ResultSet res = st.executeQuery();
-			
-			while (res.next()) {
-				result.add(res.getString("i.Type"));
-			}
 			
 			res.close();
 			st.close();
@@ -128,8 +99,88 @@ public List<String>  getPeso (String gId1, String gId2) {
 			throw new RuntimeException("Database error", e) ;
 		}
 	}
+	
+//	
+//	// prendo i distinct type di due geni  appartenenti   a due localita 
+//public String  getType2Genes ( String g1, String g2) {
+//		
+//		String sql = " select distinct i.`Type` "
+//				+ "from interactions i, genes g1, genes g2  "
+//				+ "where g1.`GeneID`= i.`GeneID1` and g2.`GeneID`= i.`GeneID2` and "
+//				+ "g1.`GeneID`= ?  and g2.`GeneID`= ? "; 
+//		
+//	// G1 viene da localization 1
+//	// G2 viene da localization 2
+//		
+//		String result = null; 
+//		Connection conn = DBConnect.getConnection();
+//
+//		try {
+//			
+//			PreparedStatement st = conn.prepareStatement(sql);
+//			st.setString(1,g1); 
+//			st.setString(2,g2);
+//
+//			ResultSet res = st.executeQuery();
+//			
+////			res.first();
+//			if(res.isFirst())
+//				result= res.getString("i.Type");  
+//			
+//			
+//			res.close();
+//			st.close();
+//			conn.close();
+//			return result;
+//			
+//		} catch (SQLException e) {
+//			throw new RuntimeException("Database error", e) ;
+//		}
+//	}
 
 	
+public List<Interactions> collegamentoArchi(String l1, String l2, Map<String,Genes> idMap) {
+	
+
+	String sql = "  select distinct  i.* "
+			+ " from interactions i, genes g1, genes g2, classification c1, classification c2 "
+			+ " where g1.`GeneID`= i.`GeneID1` and g2.`GeneID`= i.`GeneID2` and c1.`GeneID`= g1.`GeneID` "
+			+ " and c2.`GeneID`= g2.`GeneID`and c1.`Localization`=? and c2.`Localization`=? "; 
+	
+			//  5   11
+	
+	List<Interactions> result = new ArrayList<Interactions>();
+	//boolean result= false; 
+	Connection conn = DBConnect.getConnection();
+
+	try {
+		
+		PreparedStatement st = conn.prepareStatement(sql);
+		st.setString(1, l1);
+		st.setString(2, l2);
+
+		ResultSet res = st.executeQuery();
+		
+	
+		while (res.next()) {
+			
+			Interactions inter = new Interactions (idMap.get(res.getString("i.GeneID1")), idMap.get(res.getString("i.GeneID2")),res.getString("i.Type"),
+				                 res.getDouble("i.Expression_Corr"));
+			
+			result.add(inter);
+		}
+		res.close();
+		st.close();
+		conn.close();
+		return result;
+		
+	} catch (SQLException e) {
+		throw new RuntimeException("Database error", e) ;
+	}
+}
+
+
+
 
 	
 
--- src/main/java/it/polito/tdp/genes/model/Model.java
+++ src/main/java/it/polito/tdp/genes/model/Model.java
@@ -20,6 +20,7 @@ public class Model {
     
     Map <String,Genes>idMapGenes= new HashMap<>();
     private List<String> bestCammino ; 
+    
    
     
 
@@ -29,6 +30,7 @@ public class Model {
     	
     	this.dao= new GenesDao();  
     	this.graph= new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+    	
     	this.allLocalization= new ArrayList<>();
     	
     	
@@ -41,11 +43,12 @@ public class Model {
 		 
 		 
     	allLocalization= dao.getAllCromosomi(); 
+    	dao.getAllGenes(idMapGenes); 
+    	
 
 		 /** VERTICI */
 	    	Graphs.addAllVertices(this.graph, allLocalization);
 	 		System.out.println("NUMERO vertici GRAFO: " +this.graph.vertexSet().size());
-	 		
 	 		
 	 		/*
  esiste un  arco se e solo  se esiste almeno una interazione che coinvolge due geni, 
@@ -64,16 +67,11 @@ PESO:  dovrà essere un numero intero, pari al numero di ** TYPE diversi di inte
 	 				
 	 				if(l1.compareTo(l2)!=0) { 
 	 					
-	 					List<String> listGenesLocal1= dao.getAllGenesLocal(l1);
-	 					List<String> listGenesLocal2= dao.getAllGenesLocal(l2);
+//	 				 int peso= getPesoType(l1,l2); 
+	 					int peso = getNewPesoType(l1,l2); 
 	 				
-
-	 					
-	 				int peso= getPesoType(listGenesLocal1,listGenesLocal2); 
-	 				if(peso>0) {
-	 					System.out.println("\n peso creato\n");
+	 				 if(peso>0) {
  						Graphs.addEdgeWithVertices(this.graph, l1, l2, peso);
- 						//this.listPesi.add(peso); //abbastanza inutile, usa i GRAFI
 	 				}
 	 			}
 	 				
@@ -89,31 +87,64 @@ PESO:  dovrà essere un numero intero, pari al numero di ** TYPE diversi di inte
     // se esiste almeno una interazione che coinvolge due geni di due localz diverse
     
     
-	private int getPesoType(List<String> listGenesLocal1, List<String> listGenesLocal2) {
-	
-		List<String> temp= new ArrayList<>(); 
-		
-		
-		for(String g1:listGenesLocal1 ) {
-			for(String g2:listGenesLocal2 ) {
-				
-				for(String c1:  dao.getPeso(g1, g2)) {
-					if(!temp.contains(c1)) {
-						temp.add(c1);
-						
-					}
-				}
-				
-			}
-		}
-		
-		
+    
+    
+    
+    // do in input due localita
+	private int getNewPesoType(String l1, String l2) {
+
+		 List<Interactions> allInteractions2Location= dao.collegamentoArchi(l1,l2, idMapGenes); 
+		 Map<String,Integer> temp= new HashMap<>(); 
+
+		 
+		 for(Interactions ii: allInteractions2Location) {
+			 if(temp.get(ii.getType())==null) {
+					temp.put(ii.getType(), 0);  // qui metto tutti i TYPE differenti	
+				}		
+		 }
+		 
 		return temp.size();
 	}
+
+
+
+/** CON QUESTO METODO NON SI TROVA **/ 
+//	private int getPesoType(String l1, String l2) {
+//
+//	 List<String> listGenesLocal1= dao.getAllGenesLocal(l1);  // tutti i geni appartenenti a localizzazione 1
+//	 List<String> listGenesLocal2= dao.getAllGenesLocal(l2);  // tutti i geni appartenenti a localizzazione 2
+//	
+//	 Map<String,Integer> temp= new HashMap<>(); 
+//	 
+//
+//	 /** calcolo le interazioni tra due diverse localita' -->  **/
+//	 
+//	 for(String g1:listGenesLocal1 ) {
+//			for(String g2:listGenesLocal2 ) {
+//				
+//				if( g1.compareTo(g2)!=0 ) {
+//					String tt= dao.getType2Genes(g1,g2);
+//					
+//					if(temp.get(tt)==null) {
+//						temp.put(tt, 0);  // qui metto tutti i TYPE differenti	
+//					}		
+//				}		
+//			}
+//	 }
+//				
+//		return temp.size(); 
+//	}
+	
+	
+	
+
 	
 	
 	
 	
+	
+	
+		
 	
 	
 }
